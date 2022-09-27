d8a1dd9856da9e3648c1752feee1a0d746fd416e
Phạm Thanh <thanhphamhumg@gmail.com>
2022-09-27 17:03:22 +0700

    baitap

--- src/Hien_Thi_20_So_NguyenTo.java
+++ src/Hien_Thi_20_So_NguyenTo.java
@@ -2,26 +2,25 @@ import java.util.Scanner;
 
 public class Hien_Thi_20_So_NguyenTo {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("5. In ra 20 số nguyên tố đầu");
-        int bienDem=0;
-        for ( int count=2;count>0;count++){
-            boolean check = true;
-            int i=2;
-            while (i <count) {
-                if (count % i == 0) {
+
+        boolean check ;
+        int N = 2;
+        int count = 0;
+        while (count < 20) {
+            check = true;
+            for (int i = 2; i < N; i++)
+                if (N % i == 0) {
                     check = false;
                     break;
                 }
-                i++;
-            }
             if (check) {
-                System.out.println(count);
-                bienDem++;
+                System.out.printf("%d ",N);
+                count++;
             }
-            if (bienDem==20){
-               break;
-            }
+            N++;
         }
+
     }
 }
